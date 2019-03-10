33c8273634c2b9d21f806dd257bcb83af445f4b7
nikhileshtippana <nikhilesh.nike@gmail.com>
2019-03-09 23:13:28 -0500

    Added CORS Filter

---
+++ src/main/java/com/nik/rest/app/config/CorsFilter.java
@@ -0,0 +1,31 @@
+package com.nik.rest.app.config;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CorsFilter implements Filter {
+
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Max-Age", "180");
+        chain.doFilter(servletRequest, servletResponse);
+		
+	}
+
+}
